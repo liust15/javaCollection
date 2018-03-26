81719b6cf873dcb9a4e7934e0ac68615531c576d
liust15 <1316431084@qq.com>
2018-03-26 16:44:25 +0800

    完成filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,31 +2,62 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-
-       // throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) % 2 == 0) {
+                list.add(array.get(i));
+            }
+        }
+        return list;
+        // throw new NotImplementedException();
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) % 3 == 0) {
+                list.add(array.get(i));
+            }
+        }
+        return list;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < firstList.size(); i++) {
+            for (int j = 0; j < secondList.size(); j++) {
+                if (firstList.get(i) == secondList.get(j)) {
+                    list.add(firstList.get(i));
+                    break;
+                }
+            }
+        }
+        return list;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (!list.contains(array.get(i))) {
+                list.add(array.get(i));
+            }
+        }
+        return list;
     }
 }
